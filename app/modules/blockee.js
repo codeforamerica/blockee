79cde615fb5da1cd7cea97069d8dd7085c05928b
Serena Wales <serenawr@gmail.com>
2012-08-10 11:58:05 -0700

    Merge fixes

--- app/modules/blockee.js
+++ app/modules/blockee.js
@@ -8,11 +8,8 @@ define([
   "googlylogo",
   "models",
   "googlystreetview",
-<<<<<<< Updated upstream
-  "sharefeature"
-=======
+  "sharefeature",
   "iframetransport"
->>>>>>> Stashed changes
 ],
 
 function(app, Backbone, Kinetic, Googlylogo, Models, GooglyStreetView, ShareFeature) {
@@ -388,7 +385,7 @@ function(app, Backbone, Kinetic, Googlylogo, Models, GooglyStreetView, ShareFeat
     ShareFeature.show();
   }
 
-  function uploadFile(e) {
+  function handleFormSubmit(e) {
 
   }
 
@@ -404,15 +401,12 @@ function(app, Backbone, Kinetic, Googlylogo, Models, GooglyStreetView, ShareFeat
      
     events: {
       "click #street-view": handleStreetViewClick,
-<<<<<<< Updated upstream
-      "click #share-button": handleShareClick
-=======
-      "submit #upload-form": uploadFile
+      "click #share-button": handleShareClick,
+      "submit #upload-form": handleFormSubmit
     },
 
     testFunc: function() {
       console.log("test");
->>>>>>> Stashed changes
     },
 
     initialize: function(options) {
