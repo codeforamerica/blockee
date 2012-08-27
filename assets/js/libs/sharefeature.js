7c3baeda505b029ac754e0101e07e91661bfa6c4
Nick Doiron <ndoiron@mapmeld.com>
2012-08-27 15:11:12 -0700

    randomize Tweet text

--- assets/js/libs/sharefeature.js
+++ assets/js/libs/sharefeature.js
@@ -10,7 +10,18 @@ var ShareFeature = {
     var url = "http://api.bitly.com/v3/shorten?longUrl=" + encodeURIComponent(longUrl) + "&login=o_cdttlflq9&apiKey=R_54cc6bcddf5bd50607743cc8158d722f";
     var sf = this;
     $.getJSON(url, function(data) {
-      $(".twitter-share-iframe")[0].src="//platform.twitter.com/widgets/tweet_button.html?count=none&text=" + encodeURIComponent("Can we make this happen?") + "&url=" + encodeURIComponent(data.data.url);
+      var messages = [
+        // "Your message ",
+        "Can we make this happen?",
+        "Rock your block with some neighborhood bling at",
+        "Bling your block! Help better your hood at",
+        "It's a blingy block party! Better your block at",
+        "If you like it, you should put bling on it! Bling your block at",
+        "Diamonds are forever. Bling is for the clever.",
+        "Paving the streets with gold is so yesterday:"
+      ];
+      var textPrompt = messages[Math.floor(Math.random() * messages.length)];
+      $(".twitter-share-iframe")[0].src="//platform.twitter.com/widgets/tweet_button.html?count=none&text=" + encodeURIComponent(textPrompt) + "&url=" + encodeURIComponent(data.data.url);
       //$(".facebook-share-iframe")[0].src="//facebook.com/plugins/like.php?href=" + encodeURIComponent(data.data.url) + "&send=false&layout=standard&width=450&show_faces=false&action=like&colorscheme=light&font&height=35";
       sf.shortUrl = data.data.url;
       $("#long_url").val( data.data.url );
